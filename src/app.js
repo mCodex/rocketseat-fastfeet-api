412000f5ea856448df59fab468576c3f325e4905
Mateus Andrade <mat.andrade@live.com>
2020-02-09 10:41:52 -0300

    added helmetjs

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 
 import routes from './routes';
 
@@ -6,6 +7,7 @@ import './database';
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use(routes);
 
